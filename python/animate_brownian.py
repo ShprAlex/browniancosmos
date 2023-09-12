9f60d64d5a39bac854acfb0b84c8a106ce1766ec
Alex Shapiro <shapiro.alex@gmail.com>
2023-09-12 17:41:47 -0400

    cleanup python

--- python/animate_brownian.py
+++ python/animate_brownian.py
@@ -1,73 +1,60 @@
 import pygame
 import sys
-import math
 from brownian_to_wave import Simulation
 
+
 def main():
-    
     pygame.init()
 
     # Set the dimensions of the canvas
-    canvas_width = 1200  # Width in pixels
-    canvas_height = 800  # Height in pixels
-    histogram_size = 200
-    max_population = 10
-    cell_size = max(1, 800//histogram_size)  # Size of each cell (square) in pixels
-    step_size = 5
+    canvas_width = 1200
+    canvas_height = 800
+    histogram_size = 400
+    max_population = 100
+    cell_size = max(1, canvas_height // histogram_size)  # Size of each cell (square) in pixels
+    velocity = 5
 
     # Calculate the number of columns to fill the whole width of the window
-    num_columns = canvas_width // cell_size
+    grid_width = canvas_width // cell_size
 
     # Create the Pygame screen
     screen = pygame.display.set_mode((canvas_width, canvas_height))
 
-    simulation = Simulation(max_population=max_population, histogram_size=histogram_size )
+    simulation = Simulation(
+        max_population=max_population, histogram_size=histogram_size
+    )
 
-    # Clock to control the frame rate
-    # clock = pygame.time.Clock()
-    # clock.tick(100)  # 20 frames per second (adjust as needed)
-
-    # Main loop
-    grid = []
-    count = 560
+    progress = 0
     while True:
-        count+=1
-        simulation.step(step_size)
+        progress += 1
+        simulation.step(velocity)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        n = int(pow(1.0085, abs((count)%(2060*2)-560)))
-        n = min(60,n)
-       
-        smooth = False #(count//200)%3==0
+        n = int(pow(1.0085, abs((progress + grid_width) % (grid_width * 2) - grid_width)))
+
+        smooth = False
         red_column = simulation.to_wave(n, smooth)
-        green_column = simulation.to_wave(n/2+0.5, smooth)
-        blue_column = simulation.to_wave(n/4+0.75, smooth)
-        
-        # grid.append(column)
-        # # Remove the oldest column if the grid size exceeds the number of columns to display
-        # if len(grid) > num_columns:
-        #     grid.pop(0)
-       
-        # Draw the grid
-        # screen.fill((0, 0, 0))  # Fill the screen with black
-        i = count%(num_columns)
-        #for i, col in enumerate(grid):
-        for j in range(histogram_size):
-            #value = col[j]*3+col[j-1]/2+col[j+1]/2
-            #blue, green, red = column[j]
-            red = red_column[j]
-            green = green_column[j]
-            blue = blue_column[j]
-            
-            color = (int(red * 255), int(green * 255), int(blue * 255))  # Use grayscale colors based on value
-            pygame.draw.rect(screen, color, pygame.Rect(i * cell_size, j * cell_size, cell_size, cell_size))
+        green_column = simulation.to_wave(n / 2 + 0.5, smooth)
+        blue_column = simulation.to_wave(n / 4 + 0.75, smooth)
+
+        x = progress % grid_width
+        for y in range(histogram_size):
+            red = red_column[y]
+            green = green_column[y]
+            blue = blue_column[y]
+
+            color = (int(red * 255), int(green * 255), int(blue * 255))
+            pygame.draw.rect(
+                screen,
+                color,
+                pygame.Rect(x * cell_size, y * cell_size, cell_size, cell_size),
+            )
 
         pygame.display.flip()
- 
+
 
 if __name__ == "__main__":
-
     main()
--- python/brownian_to_wave.py
+++ python/brownian_to_wave.py
@@ -1,77 +1,77 @@
 import random
 import math
 
+
 class Simulation:
     def __init__(self, max_population, histogram_size):
         self.MAX_POPULATION = max_population
         self.HISTOGRAM_SIZE = histogram_size
         self.initialize_population()
-        self.brighness = math.sqrt(self.MAX_POPULATION)/math.sqrt(self.HISTOGRAM_SIZE)
+        self.brighness = math.sqrt(self.MAX_POPULATION) / math.sqrt(self.HISTOGRAM_SIZE)
 
     def initialize_population(self):
-        self.histogram = [0]*self.HISTOGRAM_SIZE
+        self.histogram = [0] * self.HISTOGRAM_SIZE
         for _ in range(self.MAX_POPULATION):
-            self.histogram[random.randint(0,self.HISTOGRAM_SIZE-1)]+=1
+            self.histogram[random.randint(0, self.HISTOGRAM_SIZE - 1)] += 1
 
     def update_population(self):
-        hist2 = [0]*self.HISTOGRAM_SIZE
+        hist2 = [0] * self.HISTOGRAM_SIZE
         for i in range(self.HISTOGRAM_SIZE):
             for j in range(self.histogram[i]):
-                r = random.randint(-1,1)
-                hist2[(i+r)%self.HISTOGRAM_SIZE]+=1
+                r = random.randint(-1, 1)
+                hist2[(i + r) % self.HISTOGRAM_SIZE] += 1
         self.histogram = hist2
 
     def normalize_brightness(self, value, n):
-        return min(1,max(0, value/math.sqrt(n)/self.brighness/2))
+        return min(1, max(0, value / math.sqrt(n) / self.brighness / 2))
 
-    def to_wave_rough(self, n):
-        column = [0]*len(self.histogram)
+    def to_wave_square(self, n):
+        column = [0] * len(self.histogram)
         r = int(n)
-        fraction = n-r
+        fraction = n - r
 
         for i in range(self.HISTOGRAM_SIZE):
-            for j in range(-r,r):
-                v = self.histogram[(i+j)%self.HISTOGRAM_SIZE]
-                if j<0:
-                    column[i]+=v 
+            for j in range(-r, r):
+                v = self.histogram[(i + j) % self.HISTOGRAM_SIZE]
+                if j < 0:
+                    column[i] += v
                 else:
-                    column[i]-=v
+                    column[i] -= v
 
-            if fraction>0:
-                column[i]+=self.histogram[(i-r-1)%self.HISTOGRAM_SIZE]*fraction
-                column[i]-=self.histogram[(i+r)%self.HISTOGRAM_SIZE]*fraction
-                
-            column[i] = self.normalize_brightness(column[i],n)
+            if fraction > 0:
+                column[i] += (
+                    self.histogram[(i - r - 1) % self.HISTOGRAM_SIZE] * fraction
+                )
+                column[i] -= self.histogram[(i + r) % self.HISTOGRAM_SIZE] * fraction
+
+            column[i] = self.normalize_brightness(column[i], n)
 
         return column
 
-    def to_wave_smooth(self, n):
-        column = [0]*self.HISTOGRAM_SIZE
+    def to_wave_triangle(self, n):
+        column = [0] * self.HISTOGRAM_SIZE
         r = int(n)
 
         for i in range(self.HISTOGRAM_SIZE):
-            quarter = 0# r//4
-            for j in range(-r+quarter,0+quarter):
-                v = self.histogram[(i+j)%self.HISTOGRAM_SIZE]
-                k = r+j-quarter
-                column[i]+=v*(r-abs(r-1-k*2))/r
-                
-            for j in range(0-quarter,r-quarter):
-                v = self.histogram[(i+j)%self.HISTOGRAM_SIZE]
-                k = j+quarter
-                column[i]-=v*(r-abs(r-1-k*2))/r
+            for j in range(-r, 0):
+                v = self.histogram[(i + j) % self.HISTOGRAM_SIZE]
+                k = r + j
+                column[i] += v * (r - abs(r - 1 - k * 2)) / r
 
-            column[i] = self.normalize_brightness(column[i],n)
+            for j in range(0, r):
+                v = self.histogram[(i + j) % self.HISTOGRAM_SIZE]
+                k = j
+                column[i] -= v * (r - abs(r - 1 - k * 2)) / r
 
-        return column     
-    
+            column[i] = self.normalize_brightness(column[i], n)
+
+        return column
+
     def to_wave(self, n, smooth=False):
         if smooth:
-            return self.to_wave_smooth(n)
-        return self.to_wave_rough(n)
+            return self.to_wave_triangle(n)
+        return self.to_wave_square(n)
 
     def step(self, generations=1):
         for _ in range(generations):
             self.update_population()
-
-
